58ffce7ceba06308cfbe69437b85e9f86587191a
Yaroslav Lapin <ylapin@legalzoom.com>
2025-06-13 13:00:27 -0600

    rename executable

--- build.sh
+++ build.sh
@@ -14,15 +14,15 @@ MAIN_JS="build/dev/javascript/proto_tmp/gleam.main.mjs"
 bun build "$MAIN_JS" --outfile bins/bundle.js --target bun
 
 # Step 5: Compile the bundled JS to a Deno executable
-deno compile --output bins/proto_tmp --allow-all bins/bundle.js
+deno compile --output bins/tmp --allow-all bins/bundle.js
 
 rm bins/bundle.js
 
 VERSION="0.0.1"
 
-tar -czvf bins/proto_tmp_${VERSION}_darwin.tar.gz bins/proto_tmp
+tar -czvf bins/proto_tmp_${VERSION}_darwin.tar.gz bins/tmp
 
 shasum -a 256 bins/proto_tmp_${VERSION}_darwin.tar.gz > bins/proto_tmp_${VERSION}_darwin_checksums.txt
 
 
-echo "Build complete! Executable is ./proto_tmp/bins/proto_tmp" 
+echo "Build complete! Executable is ./proto_tmp/bins/tmp" 
